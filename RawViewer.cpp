f461709d8acb11c7c378746f59789cf70f7b8708
hyoungham <hyoung.ham@nauto.com>
2020-04-09 17:15:54 -0700

    adding RawImageViewer class

--- RawViewer.cpp
+++ RawViewer.cpp
@@ -1,7 +1,51 @@
-#include <SDL2/SDL.h> 
-#include <SDL2/SDL_image.h> 
-#include <SDL2/SDL_timer.h> 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+#include <fstream>
+#include <functional>
 #include <iostream>
+#include <memory>
+#include <vector>
+
+using WndDtor = std::function<void(SDL_Window *)>;
+
+class RawImageViewer {
+public:
+  RawImageViewer(int width, int height) : mWidth(width), mHeight(height) {
+    if (SDL_Init(SDL_INIT_VIDEO)) {
+      throw std::runtime_error("SDL_INIT error");
+    }
+    mWindow = std::shared_ptr<SDL_Window>(
+        SDL_CreateWindow("RawImageViewer", SDL_WINDOWPOS_CENTERED,
+                         SDL_WINDOWPOS_CENTERED, width, height,
+                         SDL_WINDOW_SHOWN),
+        [](SDL_Window *wnd) {});
+    if (mWindow == nullptr) {
+      throw std::runtime_error("SDL_CreateWindow failure");
+    }
+  }
+
+  bool eventHandle() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        SDL_Quit();
+        return false;
+      } else if (event.type == SDL_KEYDOWN) {
+        if (event.key.keysym.sym == SDLK_ESCAPE) {
+          SDL_Quit();
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+private:
+  int mWidth;
+  int mHeight;
+  std::shared_ptr<SDL_Window> mWindow;
+};
 
 int main(int argc, const char **argv) {
 
@@ -14,27 +58,9 @@ int main(int argc, const char **argv) {
   int height = std::stoi(*++argv);
   std::cout << fileName << ":" << width << "x" << height << std::endl;
 
-  if (SDL_Init(SDL_INIT_VIDEO)) {
-    throw std::runtime_error("SDL_INIT error");
-  }
-  SDL_Window *window = SDL_CreateWindow("RawViewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
-  if (window == nullptr) {
-    throw std::runtime_error("SDL_CreateWindow failure");
-  }
+  RawImageViewer viewer{width, height};
+  while (viewer.eventHandle())
+    ;
 
-  bool IsExit{false};
-  SDL_Event event;
-  while (!IsExit) {
-    while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_QUIT) {
-				IsExit = true;
-      } else if (event.type == SDL_KEYDOWN) {
-        if (event.key.keysym.sym == SDLK_ESCAPE)
-          IsExit = true;
-      }
-    }
-  }
-
-  SDL_Quit();
-  return 0; 
+  return 0;
 }
